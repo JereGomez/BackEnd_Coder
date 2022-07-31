dd834ca11087ce8fa465ff34d557f0179287a47c
JereGomez <jeregomezgiglio12@gmail.com>
2022-07-31 19:08:39 -0300

    clase9

---
+++ Clase9/desafio3/server.js
@@ -0,0 +1,49 @@
+const express=require("express");
+const handlebars=require("express-handlebars");
+const app=express();
+
+app.use(express.static("public"));
+
+
+const hbs = handlebars.create({
+    extname:".hbs",
+    defaultLayout:"index.hbs",
+    layoutsDir:__dirname+"/views/layout",
+    partialsDir:__dirname+"/views/partials/"
+});
+app.engine("hbs", hbs.engine);
+app.set('views' , './views');
+app.set('view engine' , 'hbs');
+
+
+app.get("/",(req,res)=>{
+    const user = {
+        nombre: "Antonio",
+        apellido: "Aguilar",
+        edad: 30,
+        telefono: "+504 XXXXXXX",
+        email: "jaguilar992@gmail.com",
+        comments: [
+            {text: "Qué super se ve tu sitio", user: "jlopez"},
+            {text: "Hola", user: "smorgan"}
+        ]
+    }
+    res.render('main' , user);
+});
+
+app.get('/saludo' , (req , res)=>{//ruta personalizada
+    const saludo={//objeto a mostrar
+        saludo: 'Holaaaa',
+        cuerpo: 'saludando desde el mas alla'
+    };
+    res.render('main' ,{ //renderiza main.hbs y dentro se pregunta si este o
+         saludo:true,//dentro de esta ruta el objero existe por lo que se muestra
+         ...saludo //descomprimo objeto
+    });
+});
+
+
+const PORT = 8080;
+app.listen(PORT , ()=>{
+    console.log(`Running on port ${PORT}`);
+});
---
+++ Clase9/handlebrs/server.js
@@ -0,0 +1,36 @@
+const express=require("express");
+const handlebars=require("express-handlebars");
+const app=express();
+const hbs=handlebars.create({
+    extname:".hbs",
+    defaultLayout:"index.hbs",
+    layoutsDir:__dirname+"/views/layout",
+    partialsDir:__dirname+"/views/partials/"
+});
+
+app.use(express.static("public"));
+
+app.engine("hbs",hbs.engine);
+app.set("view engine","hbs");
+app.set("views","./views");
+
+fakeApi=()=>[
+
+    {name:"Katarina",lane:"midlaner"},
+    {name:"Jayce",lane:"toplaner"},
+    {name:"Herbert",lane:"toplaner"},
+    {name:"Alan",lane:"midlaner"},
+    {name:"Bob",lane:"midlaner"},
+]
+
+app.get("/",(req,res)=>{
+    res.render("main",{
+        suggestedChamps:fakeApi(),
+        listExists:true
+    });
+});
+
+const PORT = 8080;
+app.listen(PORT , ()=>{
+    console.log(`Running on port ${PORT}`);
+});
